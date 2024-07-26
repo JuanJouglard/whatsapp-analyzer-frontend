48b51d2de63c076618115e6f8472111171304432
root <jouglardjuan@gmail.com>
2024-07-26 13:41:50 +0000

    adding key to list of messages

--- src/pages/home/chat.tsx
+++ src/pages/home/chat.tsx
@@ -3,10 +3,8 @@ import { withService, ServiceProps } from "../../services";
 import { useList } from "../../hooks/useList";
 import { Message } from "../../models/message";
 import MessageBubble from "./message";
-import { useState, Suspense } from "react";
+import { useState, Suspense, Fragment } from "react";
 import { LoadingMessage } from "../../shared";
-
-
 
 function Chat({interact, message_validation}: ServiceProps) {
     const [ messages, addMessage ] = useList<Message>([])
@@ -14,19 +12,19 @@ function Chat({interact, message_validation}: ServiceProps) {
 
     const onEnter = async (event: React.KeyboardEvent<HTMLInputElement>) => {
         if (event.key == "Enter" && message_validation?.validate_user_message(text)) {
-            addMessage({ text , id: "2", date: new Date(), response: interact?.sendQuery(text)})
+            addMessage({ text , id: new Date().toISOString(), date: new Date(), response: interact?.sendQuery(text)})
             setText("")
         }
     }
 
     return <div className="chat-container">
             <ul className="message-list" aria-label="message-list">
-                {messages.map(( message:Message ) => <>
+                {messages.map(( message:Message ) => <Fragment key={message.id}>
                                   <MessageBubble message={message} />
                                     <Suspense fallback={<LoadingMessage />}>
                                       <MessageBubble is_response={true} message={message} />
                                      </Suspense>
-                              </>)}
+                              </Fragment>)}
             </ul>
             <input
                 id="query-input"
